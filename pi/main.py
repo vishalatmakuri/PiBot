fac92e0dbe41e99adb8ac3e26dbddccd003e977d
vishalatmakuri <vishalatmakuri@github.com>
2016-01-21 05:16:39 +0000

    ¨testcode¨

--- pi/main.py
+++ pi/main.py
@@ -1,1 +1,11 @@
 # main py loop
+import time
+import picamera
+
+camera = picamera.PiCamera()
+try:
+    camera.start_preview()
+    time.sleep(10)
+    camera.stop_preview()
+finally:
+    camera.close()
