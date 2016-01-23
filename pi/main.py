5574260f8184f0db1fdad77cc908c541c71d9f85
vishalatmakuri <vishalatmakuri@github.com>
2016-01-23 04:10:22 +0000

    testcode

--- pi/main.py
+++ pi/main.py
@@ -9,3 +9,21 @@ try:
     camera.stop_preview()
 finally:
     camera.close()
+import socket
+
+HOST = 'localhost'
+PORT = 9876
+ADDR = (HOST,PORT)
+BUFSIZE = 4096
+videofile = "videos/royalty-free_footage_wien_18_640x360.mp4"
+
+bytes = open(videofile).read()
+
+print len(bytes)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+client.send(bytes)
+
+client.close()
