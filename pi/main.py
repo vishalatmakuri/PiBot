e71b24f016f288560cd8a6ca66dbbdb022989bb7
vishalatmakuri <findvishal@gmail.com>
2016-01-26 23:47:06 -0600

    test client file

--- pi/main.py
+++ pi/main.py
@@ -1,19 +1,12 @@
 # main py loop
 import time
 import picamera
-
-camera = picamera.PiCamera()
-try:
-    camera.start_preview()
-    time.sleep(10)
-    camera.stop_preview()
-finally:
-    camera.close()
-#import socket
+import socket
+import io
 #https://gist.github.com/marvin/4318413
-#HOST = 'localhost'
-#PORT = 9876
-#ADDR = (HOST,PORT)
+HOST = 'https://pibot.herokuapp.com/livefeed'
+PORT = 80
+ADDR = (HOST,PORT)
 #BUFSIZE = 4096
 #videofile = "videos/royalty-free_footage_wien_18_640x360.mp4"
 
@@ -22,8 +15,22 @@ finally:
 #print len(bytes)
 
 #client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#client.connect(ADDR)
-
+client = socket.socket()
+client.connect(ADDR)
+connection = client.makefile('wb')
+try:
+    with picamera.PiCemara() as camera:
+        camera.resolution = (640, 480)
+        camera.framerate = 24
+        # Start a preview and let the camera warm up for 2 seconds
+        camera.start_preview()
+        time.sleep(2)
+        # Start recording, sending the output to the connection for 60
+        # seconds, then stop
+        camera.start_recording(connection, format='h264')
+finally:
+    connection.close()
+    client.close()
 #client.send(bytes)
 
 #client.close()
