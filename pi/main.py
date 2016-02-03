751bd8ac1284a9630711c8f612a5f8b6f52054b9
vishalatmakuri <vishalatmakuri@github.com>
2016-02-03 04:05:07 +0000

    base off test

--- pi/main.py
+++ pi/main.py
@@ -1,36 +1,32 @@
-# main py loop
 import time
 import picamera
-import socket
 import io
+import base64
+import cv2
+import numpy as np
+import os
+from PIL import Image
+from socketIO_client import SocketIO ,BaseNamespace
 #https://gist.github.com/marvin/4318413
-HOST = 'https://pibot.herokuapp.com/livefeed'
+HOST = 'http://pibot.herokuapp.com'
 PORT = 80
-ADDR = (HOST,PORT)
-#BUFSIZE = 4096
-#videofile = "videos/royalty-free_footage_wien_18_640x360.mp4"
+nodeSocket = SocketIO(HOST, PORT)
 
-#bytes = open(videofile).read()
-
-#print len(bytes)
-
-#client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client = socket.socket()
-client.connect(ADDR)
-connection = client.makefile('wb')
-try:
-    with picamera.PiCemara() as camera:
-        camera.resolution = (640, 480)
-        camera.framerate = 24
-        # Start a preview and let the camera warm up for 2 seconds
-        camera.start_preview()
-        time.sleep(2)
-        # Start recording, sending the output to the connection for 60
-        # seconds, then stop
-        camera.start_recording(connection, format='h264')
-finally:
-    connection.close()
-    client.close()
-#client.send(bytes)
-
-#client.close()
+my_file = open('my_image.jpg', 'wb')
+with picamera.PiCamera() as camera:
+    camera.start_preview()
+    time.sleep(2)
+    camera.capture(my_file)
+# At this point my_file.flush() has been called, but the file has
+# not yet been closed
+my_file.close()
+img = Image.open('my_image.jpg').convert("RGB")
+img.save('toserver.webp',"WEBP")
+with open('toserver.webp','rb') as image_file:
+    send = base64.b64encode(image_file.read())
+    print send
+#np.fromstring(my_stream.getvalue(), dtype=uint8)
+    nodeSocket.emit('stream', 'data:image/webp;base64,'+send)
+    image_file.close()
+camera.close()
+    
