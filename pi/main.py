88f6c1d42ec4da07e5243af60bf916d190747a7c
vishalatmakuri <findvishal@gmail.com>
2016-05-10 16:22:10 -0500

    Update main.py
    clean up

--- pi/main.py
+++ pi/main.py
@@ -7,7 +7,6 @@ import numpy as np
 import os
 from PIL import Image
 from socketIO_client import SocketIO ,BaseNamespace
-#https://gist.github.com/marvin/4318413
 HOST = 'http://pibot.herokuapp.com'
 PORT = 80
 nodeSocket = SocketIO(HOST, PORT)
@@ -17,15 +16,12 @@ with picamera.PiCamera() as camera:
     camera.start_preview()
     time.sleep(2)
     camera.capture(my_file)
-# At this point my_file.flush() has been called, but the file has
-# not yet been closed
 my_file.close()
 img = Image.open('my_image.jpg').convert("RGB")
 img.save('toserver.webp',"WEBP")
 with open('toserver.webp','rb') as image_file:
     send = base64.b64encode(image_file.read())
     print send
-#np.fromstring(my_stream.getvalue(), dtype=uint8)
     nodeSocket.emit('stream', 'data:image/webp;base64,'+send)
 image_file.close()
 camera.close()
